5bcf6a025b74d23fee17d655d42905447e831cdc
DiegoAbenicio <diego.abenicio1@gmail.com>
2022-06-15 09:37:35 -0300

    Cria as Paginas

---
+++ assets/js/moveTo.js
@@ -0,0 +1,13 @@
+function goToRegister() {
+  //Move o usuario para a página de registro
+  window.location.href = "register.html";
+}
+function goToHome() {
+  window.location.href = "/Pages/home.html";
+}
+function goToAgenda(){
+  window.location.href = "/Pages/Agenda.html";
+}
+function goToCadastro(){
+  window.location.href = "/Pages/CClientes.html";
+}
